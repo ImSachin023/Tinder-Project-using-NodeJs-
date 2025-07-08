a11c4bb1f96c6e09b1562deab81d3dc8c72c9495
ImSachin023 <sachinabx3@gmail.com>
2025-07-09 01:03:18 +0530

    to handle the request using routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 const express = require("express")
 
-const app = express;
+const app = express();
 
+// to handle the request
+app.use((req,res)=>{
+    res.send("hello from the server")
+})
+// to handle the request by using routes
+app.use("/" ,(req,res)=>{
+    res.send("test from the server")
+})
+app.use("/test" ,(req,res)=>{
+    res.send("test from the server")
+})
+app.use("/hello" ,(req,res)=>{
+    res.send("hell hello bro...")
+})
+//create a server 
 app.listen("7777",()=>{
     console.log("the server can listen successfully")
 })
