acd062465800c5c098ee7ad8673fd8a8c16ce370
ImSachin023 <sachinabx3@gmail.com>
2025-07-09 23:31:06 +0530

    order of the route matters

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ const express = require("express")
 
 const app = express();
 
-// to handle the request
-app.use((req,res)=>{
-    res.send("hello from the server")
-})
 // to handle the request by using routes
-app.use("/" ,(req,res)=>{
-    res.send("test from the server")
+
+app.use("/hello/2" ,(req,res)=>{
+    res.send("hello hello bro 2 ...")
 })
+
+app.use("/hello" ,(req,res)=>{
+    res.send("hello hello bro...")
+})
+
 app.use("/test" ,(req,res)=>{
     res.send("test from the server")
 })
-app.use("/hello" ,(req,res)=>{
-    res.send("hell hello bro...")
+
+app.use("/" ,(req,res)=>{
+    res.send("this is homepage")
 })
 //create a server 
 app.listen("7777",()=>{
