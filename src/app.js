91155bc96d7b8cdfac0893cca8c3ac22708db99b
ImSachin023 <sachinabx3@gmail.com>
2025-07-10 01:18:13 +0530

    dynamic routes , with the params and query

--- src/app.js
+++ src/app.js
@@ -2,32 +2,32 @@ const express = require("express")
 
 const app = express();
 
-app.use("/" ,(req,res)=>{
-    res.send("HAHAHAHAHA")
-})
 //this will handle only GET call of /user
+app.get("/user/:userId/:name/:password",(req,res)=>{
+    console.log(req.params)
+    res.send({firstname:"Sachin",lastname:"Kumar"})
+})
 app.get("/user",(req,res)=>{
+    console.log(req.query)
     res.send({firstname:"Sachin",lastname:"Kumar"})
 })
 
-app.post("/user",(req,res)=>{
-    //save data to database
-    res.send("Data saved to database successfully")
-})
+// app.post("/user",(req,res)=>{
+//     //save data to database
+//     res.send("Data saved to database successfully")
+// })
 
-app.patch("/user",(req,res)=>{
-    //save data to database
-    res.send("updation can done !!!!!")
-})
+// app.patch("/user",(req,res)=>{
+//     res.send("updation can done !!!!!")
+// })
 
-app.delete("/user",(req,res)=>{
-    //save data to database
-    res.send("Deleted Successfully")
-})
-//this will match all the HTTP method API calls to /test
-app.use("/test" ,(req,res)=>{
-    res.send("test from the server")
-})
+// app.delete("/user",(req,res)=>{
+//     res.send("Deleted Successfully")
+// })
+// //this will match all the HTTP method API calls to /test
+// app.use("/test" ,(req,res)=>{
+//     res.send("test from the server")
+// })
 
 //create a server 
 app.listen("7777",()=>{
