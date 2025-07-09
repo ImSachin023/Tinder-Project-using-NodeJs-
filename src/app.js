ef9f32fa6d9172b4fff74faff5a6c716bdd6fd9d
ImSachin023 <sachinabx3@gmail.com>
2025-07-10 00:25:42 +0530

    EXPLORING GET POST PATCH DELETE

--- src/app.js
+++ src/app.js
@@ -2,23 +2,33 @@ const express = require("express")
 
 const app = express();
 
-// to handle the request by using routes
-
-app.use("/hello/2" ,(req,res)=>{
-    res.send("hello hello bro 2 ...")
+app.use("/" ,(req,res)=>{
+    res.send("HAHAHAHAHA")
+})
+//this will handle only GET call of /user
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Sachin",lastname:"Kumar"})
 })
 
-app.use("/hello" ,(req,res)=>{
-    res.send("hello hello bro...")
+app.post("/user",(req,res)=>{
+    //save data to database
+    res.send("Data saved to database successfully")
 })
 
+app.patch("/user",(req,res)=>{
+    //save data to database
+    res.send("updation can done !!!!!")
+})
+
+app.delete("/user",(req,res)=>{
+    //save data to database
+    res.send("Deleted Successfully")
+})
+//this will match all the HTTP method API calls to /test
 app.use("/test" ,(req,res)=>{
     res.send("test from the server")
 })
 
-app.use("/" ,(req,res)=>{
-    res.send("this is homepage")
-})
 //create a server 
 app.listen("7777",()=>{
     console.log("the server can listen successfully")
