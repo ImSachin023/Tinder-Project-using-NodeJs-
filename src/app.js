1cb23c04265eaf6d1842674fcde0610c2cf8a469
ImSachin023 <sachinabx3@gmail.com>
2025-07-15 00:36:05 +0530

    apis for delete and update

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
-app.use(express.json())
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   //Creating a new instance of the User model
@@ -17,39 +17,62 @@ app.post("/signup", async (req, res) => {
   }
 });
 
-
-app.get("/user",async (req,res) => {
+app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
-    try {
-    const users = await User.findOne({emailId : userEmail})
-    if(!users){
-      res.status(404).send("User Not Found") 
-    }
-    else{
-      res.send(users)
-    }
-  // try {
-  //   const users = await User.find({emailId : userEmail})
-  //   if(!users){
-  //     res.status(404).send("User Not Found") 
-  //   }
-  //   else{
-  //     res.send(users)
-  //   }
-      
-  } catch (error) {
-    res.status(400).send("something went wrong")    
-  }
-  })
-// Feed API - GET /feed - get all users from the database
-app.get("/feed",async (req,res) => {
   try {
-    const users = await User.find({})
-    res.send(users)
+    const users = await User.findOne({ emailId: userEmail });
+    if (!users) {
+      res.status(404).send("User Not Found");
+    } else {
+      res.send(users);
+    }
+    // try {
+    //   const users = await User.find({emailId : userEmail})
+    //   if(!users){
+    //     res.status(404).send("User Not Found")
+    //   }
+    //   else{
+    //     res.send(users)
+    //   }
   } catch (error) {
-    res.status(400).send("something went wrong")    
+    res.status(400).send("something went wrong");
   }
-  })
+});
+// Feed API - GET /feed - get all users from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    // they both are same
+    await User.findByIdAndDelete({ _id: userId });
+    // await User.findByIdAndDelete(userId);
+    res.send("User Deleted Successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    // here returnDOcument is for the sake of what data is send to DB -->"before" & "after" 
+    const user = await User.findOneAndUpdate({ _id: userId }, data, {
+      returnDOcument: "after",
+    });
+    console.log(user);
+    res.send("user updated successfully");
+  } catch (error) {
+    res.status(400).send("something went wrong");
+  }
+});
 
 connectDB()
   .then(() => {
