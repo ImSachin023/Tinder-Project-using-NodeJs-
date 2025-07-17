ce7c3a897be6ffc5b6708c306ef61d792a89860f
ImSachin023 <sachinabx3@gmail.com>
2025-07-18 02:25:20 +0530

    exploring validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
 const UserSchema = mongoose.Schema(
   {
@@ -20,12 +21,22 @@ const UserSchema = mongoose.Schema(
       lowercase: true,
       unique: true,
       trim: true,
+      validate(value){
+        if(!validator.isEmail(value)){
+          throw new Error("Invalid email address :"+ value)
+        }
+      }
     },
     password: {
       type: String,
       required: true,
       minLength: 8,
       maxLength: 50,
+       validate(value){
+        if(!validator.isStrongPassword(value)){
+          throw new Error("enter a strong password :"+ value)
+        }
+      }
     },
     age: {
       type: Number,
@@ -43,6 +54,12 @@ const UserSchema = mongoose.Schema(
       type: String,
       default:
         "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngfind.com%2Fmpng%2Fxiihwx_download-empty-profile-hd-png-download%2F&psig=AOvVaw3GhsS1mznuGiOEv9EWzt_s&ust=1752842092927000&source=images&cd=vfe&opi=89978449&ved=0CBUQjRxqFwoTCOjBsOfzw44DFQAAAAAdAAAAABAE",
+       validate(value){
+        if(!validator.isURL(value)){
+          throw new Error("invalid photo URL :"+ value)
+        }
+      }
+
     },
     about: {
       type: String,
