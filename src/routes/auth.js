a9127d175344d9fa2bc297c9f92093a0888e7875
ImSachin023 <sachinabx3@gmail.com>
2025-12-12 13:16:29 +0530

    making the cookies usable in frontend

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -48,7 +48,7 @@ authRouter.post("/login", async (req, res) => {
         expires: new Date(Date.now() + 8 * 3600000), // cookie will be removed after 8 hours
       });
 
-      res.send("Login Successfully");
+      res.send(user);
     } else {
       throw new Error("Invalid Credentials");
     }
